79c62d51e07218aeb87329f98a8598899e6bbbc7
acargorkem <acargorkem@outlook.com>
2022-02-09 16:41:38 +0300

    Implement create a tweet

---
+++ src/routes/tweet.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const TweetService = require('../services/tweet-service')
+const { tweetValidationRules } = require('../validations/tweet.validation')
+const validator = require('../validations')
+
+const router = express.Router()
+
+router.post('/', tweetValidationRules(), validator, async (req, res) => {
+  const tweetBody = req.body.tweet
+  const tweet = {
+    author: req.user.id,
+    tweet: tweetBody,
+  }
+
+  const result = await TweetService.save(tweet)
+  res.status(201).json({ result })
+})
+
+module.exports = router
