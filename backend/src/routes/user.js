abf2078af5d749fd101fbf1f76c7eb14cd9c72fa
acargorkem <acargorkem@outlook.com>
2022-02-17 13:58:13 +0300

    Impelement error handling in passport for wrong credentials

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -21,16 +21,20 @@ router.get('/', getUserRules(), validator, getUser)
 
 router.post('/register', userRegisterRules(), validator, register)
 
-router.post(
-  '/login',
-  userLoginRules(),
-  validator,
-  passport.authenticate('local'),
-  (req, res) => {
-    res.status(200)
-    return res.json({ user: req.user })
-  },
-)
+router.post('/login', userLoginRules(), validator, (req, res, next) => {
+  passport.authenticate('local', (error, user) => {
+    if (error) {
+      res.status(422)
+      return res.json({ error: error.message })
+    }
+    return req.login(user, (err) => {
+      if (err) {
+        return next(err)
+      }
+      return res.send({ user })
+    })
+  })(req, res, next)
+})
 
 router.post('/logout', (req, res) => {
   req.logOut()
