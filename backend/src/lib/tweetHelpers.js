4a339c7905da5ecc15124b99a9db0533e0a20e4b
acargorkem <acargorkem@outlook.com>
2022-02-23 19:31:30 +0300

    Fix hashtags saved with # character

--- backend/src/lib/tweetHelpers.js
+++ backend/src/lib/tweetHelpers.js
@@ -1,6 +1,8 @@
 const findHashtags = (text) => {
   const pattern = /(^|\B)#(?![0-9_]+\b)([a-zA-Z0-9_]{1,30})(\b|\r)/g
-  return text.match(pattern)
+  const matches = text.match(pattern)
+  if (!matches) return null
+  const hashtags = matches.map((match) => match.slice(1).toLowerCase())
+  return hashtags
 }
-
 module.exports = { findHashtags }
