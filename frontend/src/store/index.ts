7f5e145fdf256f3b1cb464613d0a12ae0214b834
Levent <leventondeer@gmail.com>
2022-03-08 22:33:37 +0300

    create sample redux structure

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -11,9 +11,7 @@ const store = configureStore({
   // devTools: process.env.NODE_ENV !== 'production',
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
 export default store
