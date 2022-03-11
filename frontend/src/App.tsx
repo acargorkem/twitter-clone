3a33dcfed7129965c5ed5967aefc7d31dae4afef
Levent <leventondeer@gmail.com>
2022-03-11 13:54:19 +0300

    implement react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landpage from './components/landpage/Landpage'
 import SignInPopUp from './components/signin/SignInPopUp'
 
 function App() {
-
   return (
     <>
-      {/* <Landpage /> */}
-      <SignInPopUp />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landpage />} />
+          <Route path="/signin" element={<SignInPopUp />} />
+        </Routes>
+      </Router>
     </>
   )
 }
