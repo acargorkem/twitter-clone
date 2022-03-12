b09a20b4cdebbe4851b447a5257f450fcfb303e1
acargorkem <acargorkem@outlook.com>
2022-03-12 19:36:40 +0300

    Add dm routes on backend

--- backend/src/routes/dmConversationRoute.js
+++ backend/src/routes/dmConversationRoute.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const {
   getConversations,
   createConversation,
@@ -8,7 +7,6 @@ const {
 const router = express.Router()
 
 router.get('/', getConversations)
-
 router.post('/', createConversation)
 
 module.exports = router
