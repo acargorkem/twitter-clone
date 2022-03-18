bcb15bedfe7034c055b46832cbeab901c3426c4b
Levent Önder <leventondeer@gmail.com>
2022-03-18 22:44:07 +0300

    implement react router logic
    Co-authored-by: acargorkem <acargorkem@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PrivateRoute from './components/hoc/PrivateRoute'
 import Landpage from './pages/public/Landpage'
 import Loginpage from './pages/public/Loginpage'
+import Homepage from './pages/private/Homepage'
+import PublicRoute from './components/hoc/PublicRoute'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Landpage />} />
-          <Route path="/signin" element={<Loginpage />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<Landpage />} />
+            <Route path="/signin" element={<Loginpage />} />
+          </Route>
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/home" element={<Homepage />} />
+          </Route>
         </Routes>
       </Router>
     </>
