acc8a3977b02075d216ab1037676445d288ae3a4
Levent Önder <leventondeer@gmail.com>
2022-03-31 22:58:37 +0300

    implement layout factory
    Co-authored-by: acargorkem <acargorkem@users.noreply.github.com>

---
+++ frontend/src/pages/private/Explorepage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Explorepage = () => {
+  return <div>Explorepage</div>
+}
+
+export default Explorepage
---
+++ frontend/src/pages/private/Notificationpage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Notificationpage = () => {
+  return <div>Notificationpage</div>
+}
+
+export default Notificationpage
