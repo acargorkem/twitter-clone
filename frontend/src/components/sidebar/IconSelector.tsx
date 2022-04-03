7744bc3fad5fbf38bdc6140c59bf0e1206730e69
Levent Önder <leventondeer@gmail.com>
2022-04-03 19:42:25 +0300

    adjust searchbox for new layout feature

--- frontend/src/components/sidebar/IconSelector.tsx
+++ frontend/src/components/sidebar/IconSelector.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 interface navIconProps {
   SelectedIcon: React.ElementType
+  selected?: boolean
 }
 
-const IconSelector: React.FC<navIconProps> = ({ SelectedIcon }) => {
-  return <SelectedIcon fontSize="large" sx={{ color: 'grey.800' }} />
+const IconSelector: React.FC<navIconProps> = ({ SelectedIcon, selected }) => {
+  return (
+    <SelectedIcon
+      fontSize="large"
+      sx={{ color: selected ? 'grey.900' : 'grey.800' }}
+    />
+  )
 }
 
 export default IconSelector
