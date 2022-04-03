92c50d9267e39fd756116c28474877e98dfebe79
Levent Önder <leventondeer@gmail.com>
2022-04-03 19:41:48 +0300

    switch theme for css variants

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,16 @@
 import { createTheme } from '@mui/material/styles'
 
 // A custom theme for this app
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1d9bf0',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+  },
+})
 
 export default theme
